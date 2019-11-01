f234a465288686f5c0f54b50b8002f50461cd1a3
Alexandr Barulev <huston.mavr@gmail.com>
2019-11-01 18:22:51 +0300

    Fix detecting tarantool version (#4)
    * Fix detecting tarantool version
    
    * Cleanup other places retreiving version

--- ddl/db.lua
+++ ddl/db.lua
@@ -1,12 +1,7 @@
 #!/usr/bin/env tarantool
 
-local function version()
-    return require('tarantool').version
-end
-
 local function check_version(expected_major, expected_minor)
-    local db_ver = version()
-    local db_major, db_minor = db_ver:match('^(%d+)%.(%d+)')
+    local db_major, db_minor = string.match(_TARANTOOL, '^(%d+)%.(%d+)')
     local major, minor = tonumber(db_major), tonumber(db_minor)
 
     if major < expected_major then
@@ -35,7 +30,6 @@ end
 
 return {
     json_path_allowed = json_path_allowed,
-    version = version,
     varbinary_allowed = varbinary_allowed,
     multikey_path_allowed = multikey_path_allowed,
 }
