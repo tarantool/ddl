1b53e778db2772988341ebe516bcdcd24a21e9be
Sergey Bronnikov <estetus@gmail.com>
2021-07-02 18:00:23 +0300

    Remove dead code (#66)
    In commit "Top-level architecture refactoring"
    (9171f72eba03c0478f1108bd280fac19311a4c84)
    some functions were left commented out.
    This commit cleanup dead code.
    
    Closes #63

--- ddl/get.lua
+++ ddl/get.lua
@@ -33,19 +33,6 @@ local function _get_index_parts(space, index_part)
     return ddl_index_part
 end
 
--- local function _get_index_function(index_func)
---     error('Not implemented yet')
---     local ddl_func_info = {}
---     local box_func_info = box.func[index_func.name]
-
---     ddl_func_info.name = index_func.name
---     ddl_func_info.body = box_func_info.body
---     ddl_func_info.is_deterministic = box_func_info.is_deterministic
---     ddl_func_info.is_sandboxed = box_func_info.is_sandboxed
---     ddl_func_info.opts = {is_multikey = box_func_info.is_multikey}
---     return ddl_func_info
--- end
-
 local function _get_index(box_space, box_index)
     local ddl_index = {}
     ddl_index.name = box_index.name
@@ -62,17 +49,6 @@ local function _get_index(box_space, box_index)
         ddl_index.distance = box.space._index:get({box_space.id, box_index.id}).opts.distance or 'euclid'
     end
 
-    -- TODO implement later
-    -- if box_index.sequence_id ~= nil then
-    --     ddl_index.sequence = box.sequence[box_index.sequence_id]
-    --     ddl_index.sequence.uid = nil
-    --     ddl_index.sequence.id = nil
-    -- end
-
-    -- ddl_index.func = nil
-    -- if box_index.func ~= nil then
-    --     ddl_index.func = _get_index_function(box_index.func)
-    -- end
     return ddl_index
 end
 
@@ -111,17 +87,6 @@ local function get_space_schema(space_name)
     return space_ddl
 end
 
--- local function get_schema()
---     local schema = {}
-
---     for _, space in box.space._space:pairs({box.schema.SYSTEM_ID_MAX }, {iterator = "GE"}) do
---         schema[space.name] = get_space_ddl(box.space[space.name])
---     end
-
---     return schema
--- end
-
 return {
-    -- get_schema = get_schema,
     get_space_schema = get_space_schema,
 }
